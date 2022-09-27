335c0c9c6ebdbab250d3ce12817cd102019bdfb7
zehraatessonmez <49163485+zehraatessonmez@users.noreply.github.com>
2022-09-27 14:22:38 +0300

    Add files via upload

---
+++ Odev1.kt
@@ -0,0 +1,32 @@
+package com.zehraatessonmez.kotlindersleribootcamp.degisken_olusturma
+
+fun main() {
+
+    var islemNo:Int = 201
+    var ogrNo:Int = 34
+    var kitapNo:Int = 234
+    var aTarih:String = "23.03.2023"
+    var vTarih:String = "26.03.2023"
+
+    var ograd = "Ayşe"
+    var ogrSoyad:String = "Beyaz"
+    var cinsiyet:Char = 'K'
+    var dTarih:String = "23.03.2023"
+    var sinif:String = "12A"
+
+
+    var isbnNo:Int = 23
+    var kitapAdi:String = "Kumarbaz"
+    var yazarNo:Int = 25
+    var turNo:Int = 36
+    var sayfaSayisi:Int = 234
+    var puan:Double = 7.5
+
+    var yazarAdi:String = "Fyodor"
+    var yazarSoyad:String = "Dostoyevski"
+
+    var turAdi:String = "Roman"
+
+
+
+}
